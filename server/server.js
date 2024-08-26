603688cb3ec12b6c675730e180008ecac470dd07
Manikanta <134987333+soyyomani@users.noreply.github.com>
2024-08-26 22:41:12 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,32 +1,44 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
+const connectDb = require("./config/db");
 
 dotenv.config();
 
-const connectDb = require("./config/db");
 const app = express();
-const path = require('path');
 
-app.use(cors());
-app.use(express.json());
+ 
+app.use(express.json()); 
 
+ 
+const corsOptions = {
+    origin: ["http://localhost:3000", "https://online-quiz-app-1tzo.onrender.com"],  
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,  
+};
+app.use(cors(corsOptions));
+
+ 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/quiz', require('./routes/quiz'));
 app.use('/api/profile', require('./routes/profile'));
 
-const corsOptions = {
-    origin: "http://localhost:3000",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-};
+ 
+if (process.env.NODE_ENV === 'production') {
+  
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use(cors(corsOptions));
+ 
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
+ 
 const PORT = process.env.PORT || 5000;
-
 connectDb().then(() => {
     app.listen(PORT, () => {
-        console.log(`Server is running on ${PORT}`);
+        console.log(`Server is running on port ${PORT}`);
     });
 });
