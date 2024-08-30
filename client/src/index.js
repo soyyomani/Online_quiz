e3f1826c1890fd24fd7221b78f7daf220ef2382b
Manikanta <134987333+soyyomani@users.noreply.github.com>
2024-08-30 11:25:14 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
 import {AuthProvider} from './context/AuthContext';  // Default import
 import {QuizProvider} from './context/QuizContext';  // Default import
 
